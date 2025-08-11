b4b386f01c67df5ac0402bfa9f15ffa0a49a8d3b
Varun Teja <tejavarun2573@gmail.com>
2025-08-11 10:31:22 +0530

    modified llm import

--- Personal Knowledge Assistant/src/query.py
+++ Personal Knowledge Assistant/src/query.py
@@ -1,6 +1,6 @@
 from langchain.vectorstores import FAISS
 from langchain.chains import RetrievalQA
-from langchain.llms import GoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.embeddings import GoogleGenerativeAIEmbeddings
 
 GOOGLE_API_KEY ="AIzaSyDkTqFKkphvtqD4rmmFiEaSrPSNp7-tws4"
@@ -11,7 +11,7 @@ def load_chain():
 
     retriever = vectorstore.as_retriever(search_kwargs={"k": 5})
 
-    llm = GoogleGenerativeAI(model="models/gemini-2.0-flash", google_api_key=GOOGLE_API_KEY)
+    llm = ChatGoogleGenerativeAI(model="models/gemini-2.0-flash", google_api_key=GOOGLE_API_KEY)
 
     qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever, return_source_documents=True)
     return qa_chain
